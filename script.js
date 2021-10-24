22f27667c58a192d98466bf0cc8ace57a708b6f6
SpecNot <57253121+SpecNot@users.noreply.github.com>
2021-10-24 23:52:32 +1100

    version 1

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
+  if(slideIndex == 5){
+    console.log("test")
+    window.location.href = './home.html';
+
+    return
+  }
   showSlides(slideIndex += n);
+  
 }
 
 function currentSlide(n) {
@@ -13,6 +20,13 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+
+  if(slideIndex == 5){
+    document.getElementById("main-button").innerHTML = 'I Love This Name! Let’s Start!'
+  }else{
+    document.getElementById("main-button").innerHTML = 'Continue'
+  }
+
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
