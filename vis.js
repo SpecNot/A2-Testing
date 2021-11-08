6d05c51b8869386edf839d02e840120205227a50
SpecNot <57253121+SpecNot@users.noreply.github.com>
2021-11-09 02:28:38 +1100

    jkl

--- vis.js
+++ vis.js
@@ -7,69 +7,81 @@ var player_5= document.getElementById('player_5');
 var player_6 = document.getElementById('player_6');
 var player_7 = document.getElementById('player_7');
 
+let segments = [[0,175],[175,240],[240,479]]
+let startingValues = [0,175,240]
+let colours = ["red","blue","blue_d","green","orange","purple","yellow"]
+let players = []
 
-lottie.loadAnimation({
+players.push(lottie.loadAnimation({
   wrapper: player_1,
   animType: 'svg',
   loop: true,
-  path: './animation/green.json',
+  path: './animation/'+ colours[Math.floor(Math.random()*colours.length)]+'.json',
   name:'player_1',
-});
+}))
 
-lottie.loadAnimation({
+players.push(lottie.loadAnimation({
     wrapper: player_2,
     animType: 'svg',
     loop: true,
-    path: './animation/purple.json',
+    path: './animation/'+ colours[Math.floor(Math.random()*colours.length)]+'.json',
     name:'player_2',
-  });
+  }))
 
-lottie.loadAnimation({
+  players.push(lottie.loadAnimation({
     wrapper: player_me,
     animType: 'svg',
     loop: true,
     path: './animation/' + localStorage.getItem("color") + ".json",
     name:'player_me'
-  });
+  }))
 
-  lottie.loadAnimation({
+  players.push(lottie.loadAnimation({
     wrapper: player_3,
     animType: 'svg',
     loop: true,
-    path: './animation/purple.json',
+    path: './animation/'+ colours[Math.floor(Math.random()*colours.length)]+'.json',
     name:'player_3',
-  });
-  lottie.loadAnimation({
+  }))
+  players.push(lottie.loadAnimation({
     wrapper: player_4,
     animType: 'svg',
     loop: true,
-    path: './animation/purple.json',
+    path: './animation/'+ colours[Math.floor(Math.random()*colours.length)]+'.json',
     name:'player_4',
-  });
-  lottie.loadAnimation({
+  }))
+  players.push(lottie.loadAnimation({
     wrapper: player_5,
     animType: 'svg',
     loop: true,
-    path: './animation/purple.json',
+    path: './animation/'+ colours[Math.floor(Math.random()*colours.length)]+'.json',
     name:'player_5',
-  });
-  lottie.loadAnimation({
+  }))
+  players.push(lottie.loadAnimation({
     wrapper: player_6,
     animType: 'svg',
     loop: true,
-    path: './animation/purple.json',
+    path: './animation/'+ colours[Math.floor(Math.random()*colours.length)]+'.json',
     name:'player_6',
-  });
-  lottie.loadAnimation({
+  }))
+  players.push(lottie.loadAnimation({
     wrapper: player_7,
     animType: 'svg',
     loop: true,
-    path: './animation/purple.json',
+    path: './animation/'+ colours[Math.floor(Math.random()*colours.length)]+'.json',
     name:'player_7',
-  });
+  }))
 
 
   
 setTimeout(function () {
     lottie.stop()
-  }, 10);
+  }, 100);
+
+  setTimeout(function () {
+    for(i = 0; i < players.length; i++){
+        players[i].goToAndPlay(Math.floor(Math.random() * 480))
+        console.log('test')
+  
+    }  }, 1000);
+
